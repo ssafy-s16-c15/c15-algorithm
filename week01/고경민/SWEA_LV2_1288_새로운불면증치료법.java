d01c56559ee47ebd885917705777a9d3d3bcd235
cumulus308 <rhrudals312@naver.com>
2026-07-23 21:43:39 +0900

    fix: SWEA 1288 불면증치료법 코드 수정

--- week01/고경민/SWEA_LV2_1288_새로운불면증치료법.java
+++ week01/고경민/SWEA_LV2_1288_새로운불면증치료법.java
@@ -4,16 +4,22 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int T=sc.nextInt();
         for(int tc = 1; tc <= T; tc++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            int C = sc.nextInt();
-            int D = sc.nextInt();
-            int E = sc.nextInt();
-            int a = A * E;
-            int b;
-            if (C>=E) b=B;
-            else b = B + (E-C)*D;
-            int answer = Math.min(a, b);
+            boolean[] seen = new boolean[10];
+            int N = sc.nextInt();
+            int answer = 0;
+            int count = 0;
+            while (count < 10) {
+                answer += N;
+                int temp = answer;
+                while (temp > 0) {
+                    int num = temp % 10;
+                    if (!seen[num]) {
+                        seen[num] = true;
+                        count++;
+                    }
+                    temp /= 10;
+                }
+            }
             System.out.println("#" + tc + " " + answer);
         }
     }
