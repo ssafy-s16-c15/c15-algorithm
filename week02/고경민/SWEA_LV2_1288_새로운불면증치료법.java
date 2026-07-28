94b05a2ef5d4236cba8769961e5d104a34674ce4
cumulus308 <rhrudals312@naver.com>
2026-07-28 16:30:51 +0900

    code: SWEA_LV2_1288_새로운불면증치료법

---
+++ week02/고경민/SWEA_LV2_1288_새로운불면증치료법.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int tc = 1; tc <= T; tc++) {
+            boolean[] seen = new boolean[10];
+            int N = sc.nextInt();
+            int answer = 0;
+            int count = 0;
+
+            while (count < 10) {
+                answer += N;
+                int temp = answer;
+                while (temp > 0) {
+                    int num = temp % 10;
+                    if (!seen[num]) {
+                        seen[num] = true;
+                        count++;
+                    }
+                    temp /= 10;
+                }
+                    
+            }
+
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
+}
