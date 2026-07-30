337e66acf4e0f05dbdd3b79143e786b49d37591c
0hyo <87960818+0hyo@users.noreply.github.com>
2026-07-30 22:10:24 +0900

    [Week02] yeonghyo 01 (#26)
    * [week 2] 영효 문제 풀이 1차
    
    * [week 2] 영효 문제 풀이 1차
    
    * Week01 yeonghyo 02

--- week02/김영효/SWEA_LV2_1288_새로운불면증치료법.java
+++ week02/김영효/SWEA_LV2_1288_새로운불면증치료법.java
@@ -6,20 +6,18 @@ public class SWEA_LV2_1288_새로운불면증치료법 {
 	{
 	Scanner sc = new Scanner(System.in);
 		int T;
-		T=sc.nextInt();
+		T = sc.nextInt();
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int N = sc.nextInt();
+									
 			int binary = 0;
-            
             int N_tmp;
             int cnt = 0;
 			while(binary != 0b1111111111){
                 cnt++;
                 N_tmp = N * cnt;
-//                System.out.printf("%d \n", N_tmp);
                 while(N_tmp >= 1){
-
 					binary |= (1 << (N_tmp % 10));
 					N_tmp /= 10;
 				}
---
+++ week02/김영효/SWEA_LV2_1961_숫자회전배열.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class 1961_숫자회전배열 {
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+            int[][] numbers = new int[N][N];
+            int[][] numbers_tmp1 = new int[N][N];
+            int[][] numbers_tmp2 = new int[N][N];
+            int[][] numbers_tmp3 = new int[N][N];
+            for(int i = 0; i < N; i++){	
+                for(int j = 0; j < N; j++){
+						numbers[i][j] = sc.nextInt();
+                }
+            }
+            for(int i = 0; i < N; i++){	
+                for(int j = 0; j < N; j++){
+						numbers_tmp1[j][ N - i - 1] = numbers[i][j];              		
+                }
+            }    
+            for(int i = 0; i < N; i++){	
+                for(int j = 0; j < N; j++){
+						numbers_tmp2[j][ N - i - 1] = numbers_tmp1[i][j];              		
+                }
+            }    
+			for(int i = 0; i < N; i++){	
+                for(int j = 0; j < N; j++){
+						numbers_tmp3[j][ N - i - 1] = numbers_tmp2[i][j];              		
+                }
+            }    
+
+            System.out.printf("#%d\n", test_case);
+            for(int i = 0; i < N; i++){	
+                for(int j = 0; j < N; j++){
+					System.out.printf("%d",numbers_tmp1[i][j] );
+                }
+                System.out.printf(" ");
+                for(int j = 0; j < N; j++){
+					System.out.printf("%d",numbers_tmp2[i][j] );
+                }
+                System.out.printf(" ");
+                for(int j = 0; j < N; j++){
+					System.out.printf("%d",numbers_tmp3[i][j] );
+                }
+					 System.out.println();              
+            }            
+            
+			
+		}
+	}
+}
+}
